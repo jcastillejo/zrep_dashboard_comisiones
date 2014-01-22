7f4d700ea1f8f8995a418ea9939a95a4169158d6
jcastillejo <jcastillejot@se.pe>
2014-01-22 15:39:04 -0500

    reportes se
    repositorio de reportes

---
+++ vista/index.php
@@ -0,0 +1,30 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+
+<div class="rep">
+	<table>
+		<tr>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+			<td></td>
+		</tr>
+	</table>
+
+</div>
+
+
+
+	
+</body>
+</html>
+
+
+
+
